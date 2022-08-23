7fa76511731645b2a78428acc5d06c59d9897b56
Víctor Tejada <105092810+MisteryPoints@users.noreply.github.com>
2022-08-23 21:02:02 +0000

    Martes

--- src/components/AppSecure.jsx
+++ src/components/AppSecure.jsx
@@ -8,7 +8,7 @@ const AppSecure = () => {
   return (
     <>
         <header className='my-10'>
-          <h1 className='text-white text-center text-4xl font-black mb-5'>Cotizador de Seguros de Auto</h1> 
+          <h1 className='text-white text-center text-4xl font-black mb-5 cursor-default'>Cotizador de Seguros de Auto</h1> 
         </header>
         <main className='bg-white md:w-2/3 lg:w-2/4 mx-auto shadow rounded-lg p-10'>
             <Formulario/>
--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,9 +1,45 @@
- import useQuote from "../hooks/useQuote" 
+import { useState, useCallback, useMemo, useRef } from "react"
+import useQuote from "../hooks/useQuote" 
+import { MARCAS, PLANS } from '../constants'
 
 const Result = () => {
-    const {result} = useQuote()
+    const {result, datos} = useQuote()
+    const {marca, year, plan} = datos
+    const [modal, setModal] = useState(true);
+    const [nameMarca] = useCallback(MARCAS.filter(m => m.id === Number(marca))
+    ,[result]) 
+    const [namePlan] = useCallback(PLANS.filter(p => p.id === Number(plan)) 
+    ,[result]) 
+    const yearRef = useRef(year)
+    const handleX = () => { 
+        setModal(false)
+    }
+
+    if (!modal) return
+
     return (
-        <div>{result}</div>
+        <div className="bg-gray-100 text-center mt-5 p-5 shadow-sm relative shadow-gray-700 rounded-md">
+            <h2 className="text-gray-600 font-bold  text-3xl">
+                <u>Resumen</u>
+            </h2>
+            <p className="absolute right-[2%] top-[-2%] font-bold text-xl cursor-pointer" onClick={handleX}>x</p>
+            <p className="my-2">
+                <span className="font-bold">Marca:   </span>
+                {nameMarca.nombre}
+            </p>
+            <p className="my-2">
+                <span className="font-bold">Plan:   </span>
+                {namePlan.nombre}
+            </p>
+            <p className="my-2">
+                <span className="font-bold">Año del Auto:   </span>
+                {yearRef.current}
+            </p>
+            <p className="my-2 text-xl">
+                <span className="font-bold">Total de Cotización:   </span>
+                {result}
+            </p>
+        </div>
     )
 }
 
