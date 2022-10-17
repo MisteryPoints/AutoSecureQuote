c6a260e06aa24e1c9643efff01361014bcdb9a5c
MisteryPoints <misterypoint2@gmail.com>
2022-10-17 10:52:09 -0400

    Adding Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { QuoteProvider } from './context/QuoteProvider'
 import AppSecure from './components/AppSecure' 
+import { Footer } from './components/Footer'
 
 function App() { 
 
   return ( 
     <QuoteProvider> 
       <AppSecure/> 
+      <Footer/>
     </QuoteProvider>
   )
 }
---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+ 
+export function Footer() {
+    return (
+      <footer className='bg-sky-500 text-center py-3 w-full fixed bottom-0'>
+          <p className='text-white text-xl'>Designed by DevPoint Víctor Tejada</p>
+      </footer>
+    )
+  }
+  
